a98080d6f140dbe67649cb75ed53b1bd25d092ff
Arnur Atentaev <arnurarni@gmail.com>
2026-02-20 02:48:32 +0500

    made async models initializers for lifespan

--- src/agent/initialize_models.py
+++ src/agent/initialize_models.py
@@ -1,25 +1,55 @@
-# import os
-# from dotenv import load_dotenv
-# 
-# from langchain_ollama import ChatOllama
-# from sentence_transformers import CrossEncoder
-# from sentence_transformers import SentenceTransformer
-# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-# 
-# load_dotenv(".env")
-# embed_model = os.getenv("EMBEDDING_MODEL")
-# cross_enc_model = os.getenv("CROSS_ENCODER_MODEL")
-# llm_model = os.getenv("LLM_MODEL")
-# seq2seq_model = os.getenv("SEQ2SEQ_MODEL")
-# 
-# OLLAMA_PORT_SERVICE = os.getenv("OLLAMA_PORT_SERVICE")
-# OLLAMA_URL = f"http://ollama:{OLLAMA_PORT_SERVICE}"
-# 
-# embedding_model = SentenceTransformer(embed_model)
-# 
-# reranker_model = CrossEncoder(cross_enc_model)
-# 
-# generative_model = ChatOllama(model=llm_model, base_url=OLLAMA_URL)
-# 
-# seq2seq_tokenizer = AutoTokenizer.from_pretrained(seq2seq_model)
-# seq2seq_model = AutoModelForSeq2SeqLM.from_pretrained(seq2seq_model)
+import os
+import asyncio
+
+from dotenv import load_dotenv
+from langchain_ollama import ChatOllama
+from sentence_transformers import CrossEncoder
+from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+load_dotenv(".env")
+embed_model_name = os.getenv("EMBEDDING_MODEL")
+cross_enc_model_name = os.getenv("CROSS_ENCODER_MODEL")
+llm_model_name = os.getenv("LLM_MODEL")
+seq2seq_model_name = os.getenv("SEQ2SEQ_MODEL")
+
+OLLAMA_PORT_SERVICE = os.getenv("OLLAMA_PORT_SERVICE")
+OLLAMA_URL = f"http://ollama:{OLLAMA_PORT_SERVICE}"
+
+
+class ModelGetter:
+    def __init__(self, models: dict):
+        self.models = models
+
+    def __getitem__(self, key):
+        if key in self.models:
+            return self.models[key]
+        else:
+            raise KeyError(f"Model '{key}' not found.")
+
+
+async def load_models() -> dict:
+    loop = asyncio.get_running_loop()
+    
+    generative_model = ChatOllama(model=llm_model_name, base_url=OLLAMA_URL)
+
+    embedding_model = await loop.run_in_executor(
+        None, lambda: SentenceTransformer(embed_model_name)
+    )
+    reranker_model = await loop.run_in_executor(
+        None, lambda: CrossEncoder(cross_enc_model_name)
+    )
+    seq2seq_tokenizer = await loop.run_in_executor(
+        None, lambda: AutoTokenizer.from_pretrained(seq2seq_model_name)
+    )
+    seq2seq_model = await loop.run_in_executor(
+        None, lambda: AutoModelForSeq2SeqLM.from_pretrained(seq2seq_model_name)
+    )
+
+    return {
+        "embedding_model": embedding_model,
+        "reranker_model": reranker_model,
+        "generative_model": generative_model,
+        "seq2seq_tokenizer": seq2seq_tokenizer,
+        "seq2seq_model": seq2seq_model,
+    }
--- src/main.py
+++ src/main.py
@@ -1,44 +1,16 @@
-import os
 import json
-import logging
-from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 from sqlalchemy import text
 
-from langchain_ollama import ChatOllama
-from sentence_transformers import CrossEncoder
-from sentence_transformers import SentenceTransformer
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from agent.initialize_models import ModelGetter, load_models
 
 from agent.graph import build_graph, gen_png_graph
 from core.models import Base, db_helper
 from agent.views import agent_router
 from database_api import router as router_actions
-
-logging.basicConfig(level=logging.INFO)
-
-load_dotenv(".env")
-embed_model_name = os.getenv("EMBEDDING_MODEL")
-cross_enc_model_name = os.getenv("CROSS_ENCODER_MODEL")
-llm_model_name = os.getenv("LLM_MODEL")
-seq2seq_model_name = os.getenv("SEQ2SEQ_MODEL")
-
-OLLAMA_PORT_SERVICE = os.getenv("OLLAMA_PORT_SERVICE")
-OLLAMA_URL = f"http://ollama:{OLLAMA_PORT_SERVICE}"
-
-
-class ModelContainer:
-    def __init__(self, models: dict):
-        self.models = models
-
-    def __getitem__(self, key):
-        if key in self.models:
-            return self.models[key]
-        else:
-            raise KeyError(f"Model '{key}' not found.")
 
 
 @asynccontextmanager
@@ -50,27 +22,8 @@ async def lifespan(app: FastAPI):
     with open("agent/intents.json") as f:
         app.state.intents = json.load(f)
 
-    logging.info("INITIALIZING EMBEDDING MODEL")
-    embedding_model = SentenceTransformer(embed_model_name)
-    logging.info("INITIALIZING RERANKER MODEL")
-    reranker_model = CrossEncoder(cross_enc_model_name)
-    logging.info("INITIALIZING GENERATIVE MODEL")
-    generative_model = ChatOllama(model=llm_model_name, base_url=OLLAMA_URL)
-    logging.info("INITIALIZING TOKENIZER OF SEQ2SEQ MODEL")
-    seq2se2_tokenizer = AutoTokenizer.from_pretrained(seq2seq_model_name)
-    logging.info("INITIALIZING SEQ2SEQ MODEL")
-    seq2seq_model = AutoModelForSeq2SeqLM.from_pretrained(seq2seq_model_name)
-
-    mapped_model = {
-        "embedding_model": embedding_model,
-        "reranker_model": reranker_model,
-        "generative_model": generative_model,
-        "seq2seq_tokenizer": seq2se2_tokenizer,
-        "seq2seq_model": seq2seq_model,
-    }
-
-    models = ModelContainer(mapped_model)
-    app.state.models = models
+    models_map = await load_models()
+    models = ModelGetter(models_map)
 
     app.state.agent_graph = await build_graph(intents=app.state.intents, models=models)
     gen_png_graph(app.state.agent_graph, name_photo="main_graph.png")
